1bd7e8e5da2a1d18499235aba03ff302a2ee2c0e
Harsh Deep <hdeep61034@gmail.com>
2024-02-26 00:21:48 +0530

    created user signup thingy

--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -1,7 +1,45 @@
 import { Hono } from "hono";
-const user = new Hono();
+import { PrismaClient } from "@prisma/client/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
 
-user.get("/signup", (c)=>c.text("User Created Successfully"));
+const user = new Hono<{
+    Bindings: {
+		DATABASE_URL: string
+	}
+}>();
+// id String @id @default(uuid())
+// email String @unique
+// password String
+// username String @unique
+// firstName String
+// lastName String?
+// createdOn DateTime @db.Date
+// posts Post[]
+user.post("/signup", async(c)=>{
+    
+    const prisma = new PrismaClient({
+        datasourceUrl: c.env.DATABASE_URL,log: ['query']
+    }).$extends(withAccelerate())
+    
+    const body = await c.req.json()
+   
+    console.log(body);
+try{
+    const newUser = await prisma.user.create({
+        data: {
+          email: body.email,
+          password: body.password,
+          username: body.username,
+          firstName: body.firstName,
+          lastName: body.lastName,
+        },
+      });
+}
+catch(err){
+console.log("ERROR: " + err);
+}
+    return c.text("User Created Successfully");
+});
 
 user.get("/signin", (c)=>c.text("User LoggedIN"));
 
